a2230d334c93638f630fdf626c21bc8af68b7e19
0xh7ml <darkhtml2275@gmail.com>
2023-08-29 20:56:07 +0600

    update settings.py

--- drops/settings.py
+++ drops/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,3 +138,6 @@ if not DEBUG:
             'rest_framework.renderers.JSONRenderer',
         ),
     }
+
+# admin panel CSRF missing issue
+CSRF_TRUSTED_ORIGINS = ['https://*.thesaikat.xyz']
